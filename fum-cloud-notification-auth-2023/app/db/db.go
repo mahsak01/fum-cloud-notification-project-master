fd9e4e4753a441c6e4df7c5c10621ff05a2d6f8f
mahsak01 <mahsakarimi01@yahoo.com>
2024-05-17 13:46:38 +0330

    change env mysql

---
+++ fum-cloud-notification-auth-2023/app/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"auth_user/app/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(url string) Handler {
+	// dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
+	fmt.Println(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.User{})
+
+	return Handler{db}
+}
